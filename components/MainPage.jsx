0bed2d679410ae52e5ad576dfc2864d97ed5a271
FlaviaLaura27 <flavialaurapopescu@yahoo.ro>
2024-04-29 16:24:27 +0300

    docker

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -34,7 +34,7 @@ const MainPage = () => {
   };
 
   const handleUpdateRecord = (id) => {
-    router.push(`/records/edit?id=${id}`);
+    router.push(`/edit?id=${id}`);
   };
 
   useEffect(() => {
